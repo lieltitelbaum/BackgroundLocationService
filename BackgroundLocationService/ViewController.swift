b053e5dd52bd9584bdd95c3da17d2e0a2c8ec2f0
Liel Titelbaum <liel.titelbaum1@gmail.com>
2021-01-20 15:14:26 +0200

    Initial Commit

---
+++ BackgroundLocationService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundLocationService
+//
+//  Created by Liel Titelbaum on 20/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
